0ff58aa0aa4d1274fe81d2518263e050bf416b7d
AlexBrodbelt <apbrodbelt@gmail.com>
2026-01-10 20:33:08 +0100

    Remove Polychromatic from repo (already in .gitignore)

--- Lean/Polychromatic/FourThree/Combi.lean
+++
@@ -1,6 +0,0 @@
-import Polychromatic.Colouring
-
-theorem normal_bit :
-    ∀ a b c : ℤ, 0 < a → a < b → a + b ≤ c → 289 ≤ c → Finset.gcd {a, b, c} id = 1 →
-          HasPolychromColouring (Fin 3) {0, a, b, c} := by
-  sorry
